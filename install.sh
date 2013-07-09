d67e7812abcc3a90e4a37f7eeea67674c525f587
kpkerry <kevin.p.kerry@ehi.com>
2013-07-09 13:39:31 -0500

    initial commit

--- install.sh
+++ install.sh
@@ -4,7 +4,10 @@
 ## Environment Setup
 #-------------------
 CL_PARAMS=$#
-CONFDIR=/usr/share/tomcat6/webapps/conf
+##TEST##BLD_CONFDIR=/home/e482fh/ipclab-test/usr/share/tomcat6/webapps/conf
+##TEST##DEST_CONFDIR=/home/e482fh/ipclab-test/etc/tomcat6
+BLD_CONFDIR=/usr/share/tomcat6/webapps/conf
+DEST_CONFDIR=/etc/tomcat6
 #-------------------
 
 usage()
@@ -22,14 +25,25 @@ case $CL_PARAMS in
       ;;
 esac
 
-if [ -d $CONFDIR/$ENV ]; then
-   for file in `ls $CONFDIR/$ENV`
-   do
-      echo $file
-      newfile=`echo $file | sed -e 's/.$ENV//'`
-      echo $newfile
-   done
+if [ -d $BLD_CONFDIR/$ENV ]; then
+   fc=`find $BLD_CONFDIR/$ENV -type f -print 2>/dev/null | wc -l`
+
+   if [ $fc -gt 0 ]; then
+      for file in `find $BLD_CONFDIR/$ENV -type f -print 2>/dev/null`
+      do
+#         echo cp $file $DEST_CONFDIR/.
+         cp $file $DEST_CONFDIR/.
+
+         if [ $? -eq 0 ]; then
+            echo "Successfully copied file $file"
+         else
+            echo "ERROR: Problem copying $file"
+         fi
+      done
+   else
+      echo "No files found in $BLD_CONFDIR/$ENV"
+   fi
 else
-   echo "$CONFDIR/$ENV does not exist!"
+   echo "$BLD_CONFDIR/$ENV does not exist!  Please verify environment and rerun."
    exit 2
 fi
